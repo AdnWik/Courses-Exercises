084f53eca42cd3c287feb45f0a478448c763a685
Adrian Wójcik <wojcik.adrian@protonmail.com>
2023-02-12 17:24:02 +0100

    Start SQL course

---
+++ SQL Bootcamp/II/S8_Praca_z_tabelami/S8L22.sql
@@ -0,0 +1,9 @@
+CREATE TEMP TABLE user2 (
+	id          INTEGER,
+	first_name TEXT,
+	last_name   TEXT,
+	email       TEXT,
+	PRIMARY KEY (id)
+);
+
+DROP TABLE temp.user2 ;
---
+++ SQL Bootcamp/II/S8_Praca_z_tabelami/S8L24.sql
@@ -0,0 +1,3 @@
+SELECT * FROM Category;
+
+CREATE TABLE CategoryCopy AS SELECT * FROM Category;
