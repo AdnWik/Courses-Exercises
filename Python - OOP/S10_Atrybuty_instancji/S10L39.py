3f39bdfe41d3a337ca699ac9426414f6fa66c29f
Adrian Wójcik <wojcik.adrian@protonmail.com>
2023-03-28 21:09:02 +0200

    Python - OOP After lesson S10L40

---
+++ Python - OOP/S10_Atrybuty_instancji/S10L39.py
@@ -0,0 +1,26 @@
+class Book:
+    language = 'ENG'
+    is_ebook = True
+
+
+books_data = [
+    {
+        'author': 'Dan Brown', 
+        'title': 'Inferno'
+    },
+    {
+        'author': 'Dan Brown',
+        'title': 'The Da Vinci Code',
+        'year_of_publishment': 2003,
+    },
+]
+
+books = []
+for book_data in books_data:
+    book = Book()
+    for attr, value in book_data.items():
+        setattr(book, attr, value)
+    books.append(book)
+ 
+for book in books:
+    print(book.__dict__)
