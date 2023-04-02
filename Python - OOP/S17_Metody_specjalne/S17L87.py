0dbb8c9abaf36febcdbdbb69c566c743623e5d48
Adrian Wójcik <wojcik.adrian@protonmail.com>
2023-04-03 01:34:24 +0200

    Python - OOP After lesson S17L88

---
+++ Python - OOP/S17_Metody_specjalne/S17L87.py
@@ -0,0 +1,12 @@
+class Person:
+
+    def __init__(self, fname, lname) -> None:
+        self.fname = fname
+        self.lname = lname
+
+    def __repr__(self) -> str:
+        return f"{self.__class__.__name__}(fname='{self.fname}', lname='{self.lname}')"
+    
+
+person = Person('Mike', 'Smith')
+print(person)
