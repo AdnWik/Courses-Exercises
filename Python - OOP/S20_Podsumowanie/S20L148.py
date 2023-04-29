80c1c7b0acf49fafc34245536a63aa6e7e36858e
Adrian Wójcik <wojcik.adrian@protonmail.com>
2023-04-29 23:46:34 +0200

    Python - OOP After lesson S20L148

--- Python - OOP/S20_Podsumowanie/S20L148.py
+++ Python - OOP/S20_Podsumowanie/S20L148.py
@@ -20,15 +20,20 @@ class Warehouse:
         self.products = []
 
     def add_product(self, product_name, price):
-        product_names = [
-            product.product_name for product in self.products
-        ]
-        if product_name not in product_names:
+        product_names = [product.product_name for product in self.products]
+        if not product_name in product_names:
             self.products.append(Product(product_name, price))
+
+    def remove_product(self, product_name):
+        for product in self.products:
+            if product_name == product.product_name:
+                self.products.remove(product)
 
 
 warehouse = Warehouse()
 warehouse.add_product('Laptop', 3900.0)
 warehouse.add_product('Mobile Phone', 1990.0)
-warehouse.add_product('Mobile Phone', 1990.0)
+warehouse.add_product('Camera', 2900.0)
+
+warehouse.remove_product('Mobile Phone')
 print(warehouse.products)
