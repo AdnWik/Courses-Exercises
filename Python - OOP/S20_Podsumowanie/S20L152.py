ef78a6f1daabff96bca1614971e198ea78820fd9
Adrian Wójcik <wojcik.adrian@protonmail.com>
2023-05-03 10:43:17 +0200

    Python - OOP After lesson S20L152

--- Python - OOP/S20_Podsumowanie/S20L152.py
+++ Python - OOP/S20_Podsumowanie/S20L152.py
@@ -10,9 +10,6 @@ class Product:
     def __repr__(self):
         return f"Product(product_name='{self.product_name}', price={self.price})"
 
-    def __str__(self) -> str:
-        return f"Product Name: {self.product_name} | Price: {self.price}"
-
     @staticmethod
     def get_id():
         return str(uuid.uuid4().fields[-1])[:6]
@@ -32,6 +29,23 @@ class Warehouse:
             if product_name == product.product_name:
                 self.products.remove(product)
 
+    def display_products(self):
+        for product in self.products:
+            print(f'Product ID: {product.product_id} | Product name: '
+                  f'{product.product_name} | Price: {product.price}') 
 
-product = Product('Mobile Phone', 1990.0)
-print(product)
+    def sort_by_price(self, ascending=True):
+        return sorted(self.products, key=lambda product: product.price,
+                      reverse=not ascending)
+        
+    def search_product(self, query):
+        return [_ for _ in self.products if query in _.product_name]
+
+
+warehouse = Warehouse()
+warehouse.add_product('Laptop', 3900.0)
+warehouse.add_product('Mobile Phone', 1990.0)
+warehouse.add_product('Camera', 2900.0)
+warehouse.add_product('USB Cable', 24.9)
+warehouse.add_product('Mouse', 49.0)
+print(warehouse.search_product('M'))
