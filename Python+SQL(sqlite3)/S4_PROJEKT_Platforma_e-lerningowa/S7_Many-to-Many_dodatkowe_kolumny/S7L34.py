2938748680471b5067cd7a47e006885b8863b816
Adrian Wójcik <wojcik.adrian@protonmail.com>
2023-05-04 21:05:58 +0200

    Python+SQL(sqlite3) After Lesson S7L34

---
+++ Python+SQL(sqlite3)/S4_PROJEKT_Platforma_e-lerningowa/S7_Many-to-Many_dodatkowe_kolumny/S7L34.py
@@ -0,0 +1,26 @@
+import sqlite3
+
+
+conn = sqlite3.connect('Python+SQL(sqlite3)\S4_PROJEKT_Platforma_e-lerningowa\esmartdata.sqlite3')
+cur = conn.cursor()
+
+with open('Python+SQL(sqlite3)\S4_PROJEKT_Platforma_e-lerningowa\S7_Many-to-Many_dodatkowe_kolumny\S7L34.sql') as file:
+    sql = file.read()
+
+cur.executescript(sql)
+
+cur.execute('''
+            SELECT 
+            t1.created,
+            t2.title
+            FROM esmartdata_membership AS t1
+            LEFT JOIN esmartdata_course AS t2
+            ON t1.course_id = t2.id
+            WHERE t1.created BETWEEN '2021-02-01' AND '2021-03-31'
+            ''')
+
+for row in cur.fetchall():
+    print(row)
+
+conn.commit()
+conn.close()
