0ab8ea302797e507b6239f2cee1f4a1de17abbe9
Adrian Wójcik <wojcik.adrian@protonmail.com>
2023-05-05 18:25:32 +0200

    Python+SQL(sqlite3) After Lesson S7L35

---
+++ Python+SQL(sqlite3)/S4_PROJEKT_Platforma_e-lerningowa/S7_Many-to-Many_dodatkowe_kolumny/S7L35.py
@@ -0,0 +1,28 @@
+import sqlite3
+
+
+conn = sqlite3.connect('Python+SQL(sqlite3)\S4_PROJEKT_Platforma_e-lerningowa\esmartdata.sqlite3')
+cur = conn.cursor()
+
+with open('Python+SQL(sqlite3)\S4_PROJEKT_Platforma_e-lerningowa\S7_Many-to-Many_dodatkowe_kolumny\S7L35.sql', 'r', encoding='utf-8') as file:
+    sql = file.read()
+
+cur.executescript(sql)
+
+cur.execute("""
+    SELECT  "created",
+            CAST(STRFTIME('%m', "created") AS INT) AS "month",
+    CASE    
+        WHEN CAST(STRFTIME('%m', "created") AS INT) BETWEEN 1 AND 3 THEN "Q1"
+        WHEN CAST(STRFTIME('%m', "created") AS INT) BETWEEN 4 AND 6 THEN "Q2"
+        WHEN CAST(STRFTIME('%m', "created") AS INT) BETWEEN 7 AND 9 THEN "Q3"
+        ELSE 'Q4'
+    END "quarter"
+    FROM "esmartdata_membership"
+""")
+
+for row in cur.fetchall():
+    print(row)
+
+conn.commit()
+conn.close()
