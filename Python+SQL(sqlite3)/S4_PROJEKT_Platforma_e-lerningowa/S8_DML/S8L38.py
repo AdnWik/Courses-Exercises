0dce77f5ab0a0c5ab8c79316272b9bdae69f6e15
Adrian Wójcik <wojcik.adrian@protonmail.com>
2023-05-09 19:59:49 +0200

    Python+SQL(sqlite3) After Lesson S8L38

---
+++ Python+SQL(sqlite3)/S4_PROJEKT_Platforma_e-lerningowa/S8_DML/S8L38.py
@@ -0,0 +1,29 @@
+import sqlite3
+
+
+conn = sqlite3.connect('Python+SQL(sqlite3)\S4_PROJEKT_Platforma_e-lerningowa\esmartdata.sqlite3')
+cur = conn.cursor()
+
+with open('Python+SQL(sqlite3)\S4_PROJEKT_Platforma_e-lerningowa\S8_DML\S8L38.sql', 'r', encoding='utf-8') as file:
+    sql = file.read()
+
+cur.executescript(sql)
+
+record = (3, 'Mike', 'Json', 'Python Developer')
+
+
+data = (3, 'Mike', 'Json', 'Python Developer')
+cur.execute("""
+INSERT INTO esmartdata_instructor VALUES(?,?,?,?)
+""", data)
+
+conn.commit()
+
+cur.execute("""
+SELECT * FROM esmartdata_instructor
+""")
+
+for row in cur.fetchall():
+    print(row)
+
+conn.close()
